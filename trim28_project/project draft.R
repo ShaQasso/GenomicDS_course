4f043a871f6425198c74d2ca6dd3f7fd09c0e36d
CryptedSoul <Shaqed@gmail.com>
2022-02-17 21:18:48 +0200

    update

--- trim28_project/project draft.R
+++ trim28_project/project draft.R
@@ -8,6 +8,8 @@ library(ggpubr)
 library(limma)
 library(reshape2)
 library(RColorBrewer)
+library(corrplot)
+
 
 #downloaded files from expression atlas
 
@@ -24,13 +26,13 @@ metadata <-  read.table(coldata_file, row.names = 1, header = T, sep = '\t',
 
 geneNames <- as.data.frame(counts[,"Gene.Name"])
 geneNames$Gene.ID <- rownames(counts)
-geneNames <- geneNames %>% rename("Gene.Name" = 'counts[, "Gene.Name"]')
+geneNames <- geneNames %>% rename('counts[, "Gene.Name"]'= "Gene.Name")
 counts <- subset( counts, select = -Gene.Name)
 
 # get only informative columns of metadata, sex and group
 
-metadata <- metadata %>% select(Sample.Characteristic.genotype., Sample.Characteristic.sex.) %>%
-        rename(Sex = Sample.Characteristic.sex.) %>%
+metadata <- metadata %>% select('Sample.Characteristic.genotype.', 'Sample.Characteristic.sex.') %>%
+        rename('Sample.Characteristic.sex.' = "Sex") %>%
         mutate(Group = ifelse(grepl("Trim28", Sample.Characteristic.genotype.), "Trim28KO", "WT"))
 
 
@@ -41,26 +43,40 @@ metadata$lib_size = colSums(counts)
 ggplot(metadata, aes(fill = Group)) +
         geom_col(aes(x = rownames(metadata), y = lib_size)) +
         labs(title = "Barplot of library sizes") +
-        scale_x_discrete(limits = rownames(metadata)) +
-        theme_pubr()+
-        xlab(NULL)+
+        #scale_x_discrete(limits = rownames(metadata)) +
+        theme_pubr() +
+        xlab(NULL) +
         ylab("Library size") +
-        scale_fill_manual(values=met.brewer("Isfahan1", 2))+
-        theme(axis.text.x = element_text(angle=90, hjust=1))
+        scale_fill_manual(values=met.brewer("Isfahan1", 2)) +
+        facet_wrap(~Sex, scales = "free") +
+        theme(axis.text.x = element_text(angle=90, vjust=0.4))
+
+
+cpm <- apply(counts, 2, 
+             function(x) as.numeric(x)/sum(as.numeric(x)) * 10^6)
+
+
+colSums(cpm)
+
+
+keep <- rowMeans(cpm) > 0.3
+table(keep)
+
+
+counts.filtered = counts[keep,]
+dim(counts.filtered)
 
 
 
-countdata.deseq <- DESeq2::DESeqDataSetFromMatrix(countData = counts, 
-                                                  colData = metadata, 
+metadata_filtered_samples <- subset(metadata, row.names(metadata) != ("ERR4873315"))
+subset_Samples <- as.vector(rownames(metadata_filtered_samples))
+subset_reads <- counts.filtered[, subset_Samples]
+
+# create deseq object comapring Group and Sex
+
+countdata.deseq <- DESeq2::DESeqDataSetFromMatrix(countData = subset_reads, 
+                                                  colData = metadata_filtered_samples, 
                                                   design = ~ Group + Sex)
-
-
-filter <- rowSums(counts(countdata.deseq) >= 100) >= 3
-
-sum(filter)
-
-
-countdata.deseq <- countdata.deseq[filter, ]
 
 
 
@@ -119,6 +135,31 @@ heatmap.corr <- pheatmap(vsd_cor, annotation = select(metadata, c(Group, Sex)),
                          
                          color = colorRampPalette(rev(brewer.pal(n = 7, name = "RdYlBu")))(length(breaksList)),
                          main = "Correlations Heatmap")
+
+
+
+colnames(vsd_mat) <- paste(metadata$Group, metadata$Sex, sep = "_")
+
+
+correlationMatrix <- cor(vsd_mat)
+correlationMatrix
+
+#https://melyssaminto.com/blog-posts/how%20to%20change%20corrplot()%20color%20limits%20and%20color%20sc%20c5f87712fa7448948f8cf81ad1ecff56
+
+corrplot(correlationMatrix,
+         type = "upper",
+         title = "\n\n\n",
+         is.corr = TRUE,
+         order = "hclust",
+         col.lim = c(0.3,1),
+         addrect = 2,
+         #col = colorRampPalette(c("white", "deepskyblue", "blue4"))(100),
+         col = colorRampPalette(rev(brewer.pal(n = 7, name = "BrBG")))(50),
+         #addCoef.col = TRUE,
+         method = "shade",
+         tl.col = "black",
+         diag = TRUE)
+
 # Plot PCA 
 pcaData <- plotPCA(vsd, intgroup = c("Group", "Sex"), returnData = TRUE)
 
@@ -167,13 +208,8 @@ shrink_res <- lfcShrink(dds=dds_deseq, coef=2, type="apeglm")
 dds_res_all <- data.frame(shrink_res)
 
 # Subset the results to only return the significant genes with p-adjusted values less than 0.05
-dds_res_sig <- subset(dds_res_all, padj < 0.05 & abs(log2FoldChange) >= 0.58)
+dds_res_sig <- subset(dds_res_all, padj < 0.05 & abs(log2FoldChange) >= 0.5)
 
-
-
-
-# Create MA plot
-plotMA(dds_res)
 
 
 
@@ -181,8 +217,12 @@ plotMA(dds_res)
 dds_res_all <- data.frame(dds_res) %>% mutate(threshold = (padj < 0.05 & abs(log2FoldChange) >= 0.58)) %>%
         filter(!is.na(padj))
 
+
+
+dds_res_all_named <- left_join(rownames_to_column(dds_res_all), geneNames, by=c("rowname" = "Gene.ID"))
+
 # Create the volcano plot
-ggplot(dds_res_all) + 
+ggplot(dds_res_all_named) + 
         geom_point(aes(x = log2FoldChange, y = -log10(pvalue), color = threshold)) + 
         xlab("log2 fold change") + 
         ylab("-log10 adjusted p-value") + 
@@ -193,20 +233,22 @@ ggplot(dds_res_all) +
 
 
 ggplot() +
-        geom_point(data = dds_res_all, mapping = aes(x = log2FoldChange, y = -log10(pvalue)), 
+        geom_point(data = dds_res_all_named, mapping = aes(x = log2FoldChange, y = -log10(pvalue)), 
                    col = "grey80", pch = 21, size = 4) + 
-        geom_point(data = subset(dds_res_all, log2FoldChange < -0.58 & padj < 0.05),
+        geom_point(data = subset(dds_res_all_named, log2FoldChange < -0.58 & padj < 0.05),
                    aes(log2FoldChange, -log10(pvalue)), color = "red", size = 4)  +
-        geom_point(data = subset(dds_res_all, log2FoldChange > 0.58 & padj < 0.05),
+        geom_point(data = subset(dds_res_all_named, log2FoldChange > 0.58 & padj < 0.05),
                    aes(log2FoldChange, -log10(pvalue)), color = "green3", size = 4) +
         #ggtitle("in-vitro -CD3 (red) vs none (green) \nsignificant genes are colored") +
-        geom_text(data = subset(dds_res_all, abs(log2FoldChange) > 1.5 & padj < 0.05),
-                  aes(log2FoldChange, -log10(pvalue)), 
-                  label = rownames(subset(dds_res_all, abs(log2FoldChange) > 1.5 & padj < 0.05)), vjust =1, size = 4)+
+        geom_text(data = subset(dds_res_all_named, abs(log2FoldChange) > 1.5 & padj < 0.05),
+                  aes(log2FoldChange, -log10(pvalue), label = Gene.Name), 
+                  vjust =1, size = 4)+
         theme_pubr()
 
 # Subset normalized counts to significant genes
 sig_norm_counts_dds <- countdata_normalized_counts[rownames(dds_res_sig), ]
+
+
 
 # Choose heatmap color palette
 heat_colors <- brewer.pal(n = 6, name = "YlOrRd")
@@ -215,7 +257,7 @@ heat_colors <- brewer.pal(n = 6, name = "YlOrRd")
 heatmap.numClusters <- pheatmap(sig_norm_counts_dds, 
                                 #color = heat_colors, 
                                 cluster_rows = T, 
-                                show_rownames = T,
+                                show_rownames = F,
                                 #labels_row = T,
                                 annotation = select(metadata, c(Group, Sex)), 
                                 #annotation_colors = list(Condition = c('none' = "lightblue3", immunized = "red3")),
@@ -244,3 +286,6 @@ dds_res_all_full[1,]
 
 
 #random forest
+
+
+
